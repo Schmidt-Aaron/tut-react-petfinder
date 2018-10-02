7e4119fc4fa555f6e07ca46fb5bc80ef6e6f0f0f
schmidt-aaron <aschmidt808@gmail.com>
2018-10-01 22:41:40 -0700

    added petfinder API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { render } from "react-dom";
+import pf from "petfinder-client";
 import Pet from "./Pet";
 
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
 class App extends React.Component {
+  componentDidMount() {
+    const petList = petfinder.breed.list({ animal: "dog" });
+
+    petList.then(console.log, console.error);
+  }
+
   render() {
     return (
       <div>
-        <h1>Adopt Me!!</h1>
+        <h1>Adopt Me Please!</h1>
         <Pet name="Snoopy" animal="dog" breed="terrier" />
         <Pet name="Luna" animal="bird" breed="sparrow" />
         <Pet name="Doink" animal="cat" breed="mixed" />
