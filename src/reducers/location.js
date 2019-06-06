d1ad4b0a84dfdfb248c161b4b07663b1789e609e
schmidt-aaron <aschmidt808@gmail.com>
2019-06-05 21:09:38 -0700

    redux

---
+++ src/reducers/location.js
@@ -0,0 +1,10 @@
+export default function locationReducer(state = "Seattle, WA", action) {
+  // const newState = Object.assign({}, state, { location: action.payload });
+  // return newState;
+
+  if (action.type === "SET_LOCATION") {
+    return action.payload;
+  } else {
+    return state;
+  }
+}
